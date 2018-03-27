55069b11b5c453466c205eea94906cb03ebb6e9d
Alex_Turpb <turpb41@gmail.com>
2018-03-27 18:31:55 +0300

    Add visit.erb + Post & Get handlers for it, add datetimepaicker, simplecolorpicker

--- app.rb
+++ app.rb
@@ -12,7 +12,36 @@ end
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
+before do
 	@barbers = Barber.all
+end
+
+
+get '/' do
 	erb :index			
 end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username].capitalize
+	@user_phone = params[:user_phone]
+	@user_date = params[:user_date]
+	@prefered_barber = params[:prefered_barber]
+	@color = params[:color]
+
+	# hash
+	hh = {:username => 'Enter name',
+			:user_phone => 'Enter phone',
+			:user_date => 'Enter date'
+		}
+	
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+		if @error != ""
+			return erb :visit
+		else
+	end		
+end	
